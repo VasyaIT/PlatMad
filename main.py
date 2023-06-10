c4e2c0ed6d4e9e85e646da100f38881f9e2ad874
Vasya <oiuy78@yandex.ru>
2023-06-11 01:20:31 +0300

    Score and trophies

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ FPS = 60
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BARD = (200, 0, 0)
+FIOL = (162, 0, 255)
 
 game_start = True
 game_active = False
 
 block_left_img = 0
-game_duration = 0
+score = 0
 
 pygame.init()
 pygame.mixer.init()
@@ -36,31 +37,40 @@ health_surface = image_load('files/health.png')
 
 background_surface = image_load('files/background.png').convert()
 
+key = pygame.key.get_pressed()
+
 
 class Hero(Sprite):
     def __init__(self):
         super().__init__()
         self.image_right = image_load('files/characters/hero_right.png')
         self.image_left = image_load('files/characters/hero_left.png')
-        self.image = self.image_right
+        self.image_right_walk = image_load('files/characters/hero_right_walk.png')
+        self.image_left_walk = image_load('files/characters/hero_left_walk.png')
+        self.image_line = image_load('files/characters/hero_line.png')
+        self.images = [self.image_right, self.image_right_walk]
+        self.index = 0
+        self.image = self.images[self.index]
         self.rect = self.image.get_rect(midbottom=(170, 465), width=40)
         self.gravity = 0
         self.health = 10
         self.bullets = 10
 
     def hero_moves(self):
-        key = pygame.key.get_pressed()
         if key[pygame.K_w] and self.rect.bottom == 465:
             self.gravity = -15
         if key[pygame.K_d] and self.rect.x < 1000:
-            self.image = self.image_right
+            self.images = [self.image_right, self.image_right_walk]
             self.rect.x += 2
-        if key[pygame.K_a] and self.rect.x > 100:
-            if hero_img(block_left_img):
-                self.image = self.image_left
+        if key[pygame.K_a] and self.rect.x > 100 and hero_img(block_left_img):
+            self.images = [self.image_left, self.image_left_walk]
             self.rect.x -= 2
-        if key[pygame.K_s]:
-            self.rect.bottom += 2
+
+    def animation(self):
+        self.index += 0.1
+        if self.index > 1:
+            self.index = 0
+        self.image = self.images[int(self.index)]
 
     def apply_gravity(self):
         self.gravity += 0.9
@@ -70,6 +80,7 @@ class Hero(Sprite):
 
     def update(self):
         self.hero_moves()
+        self.animation()
         self.apply_gravity()
 
 
@@ -98,6 +109,8 @@ class Enemy(Sprite):
 
     def shoot(self):
         if not randint(0, 300) and self.rect.x < WIDTH:
+            if self.kind == 'godzilla':
+                self.rect.centery = randint(self.rect.top, self.rect.bottom)
             bullet = BulletEnemy(self.rect.x, self.rect.centery, self.kind)
             bullets_enemy.add(bullet)
 
@@ -168,8 +181,8 @@ class BulletEnemy(Sprite):
 
     def animation(self):
         try:
-            self.index += 5
-            if self.index > 10:
+            self.index += 1
+            if self.index > 11:
                 self.index = 0
             self.image = self.images[int(self.index)]
         except AttributeError:
@@ -196,33 +209,69 @@ bullets_hero = Group()
 bullets_enemy = Group()
 
 
+# Functions
+def score_logic():
+    if score < 5000:
+        if not randint(0, 300):
+            enemies.add(Enemy(c))
+        if score % 1000 == 0:
+            hero.sprite.bullets += 1
+    if 5000 <= score < 10000:
+        if not randint(0, 200):
+            enemies.add(Enemy(c))
+        if score % 500 == 0:
+            hero.sprite.bullets += 1
+    if score >= 10000:
+        if not randint(0, 100):
+            enemies.add(Enemy(c))
+        if score % 250 == 0:
+            hero.sprite.bullets += 1
+
+
+def trophies_draw():
+    if 1000 < score < 5000:
+        trophy_surface = image_load('files/trophies/trophy_bronz.png')
+    elif 5000 <= score < 10000:
+        trophy_surface = image_load('files/trophies/trophy_silver.png')
+    elif 10000 <= score < 20000:
+        trophy_surface = image_load('files/trophies/trophy_gold.png')
+    elif score >= 20000:
+        trophy_surface = image_load('files/trophies/trophy_leg.png')
+    else:
+        trophy_surface = None
+    return trophy_surface
+
+
 while True:
     for event in pygame.event.get():
         if (event.type == pygame.QUIT
                 or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             pygame.quit()
             exit()
-        if (
-            event.type == pygame.MOUSEBUTTONDOWN
-            and event.button == 1
-            and hero.sprite.bullets > 0
-            and game_active
-        ):
+        if game_active:
+            if (
+                event.type == pygame.MOUSEBUTTONDOWN
+                and event.button == 1
+                and hero.sprite.bullets > 0
+            ):
 
-            block_left_img = pygame.time.get_ticks()
+                block_left_img = pygame.time.get_ticks()
 
-            hero.sprite.image = hero.sprite.image_right
-            new_bullet_hero = BulletHero(hero.sprite.rect.x + hero.sprite.rect.width + 52,
-                                         hero.sprite.rect.centery - 10)
-            bullets_hero.add(new_bullet_hero)
-            hero.sprite.bullets -= 1
+                hero.sprite.image = hero.sprite.image_right
+                new_bullet_hero = BulletHero(hero.sprite.rect.x + hero.sprite.rect.width + 52,
+                                             hero.sprite.rect.centery - 10)
+                bullets_hero.add(new_bullet_hero)
+                hero.sprite.bullets -= 1
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    hero.sprite.image = hero.sprite.image_line
 
     keys = pygame.key.get_pressed()
 
     screen.blit(background_surface, (0, 0))
-    screen.blit(text_surface, (420, 30))
 
     if game_start:
+        screen.blit(text_surface, (420, 30))
         screen.blit(start_text_surface, (265, 160))
 
         if keys[pygame.K_SPACE]:
@@ -230,18 +279,9 @@ while True:
             game_active = True
 
     elif game_active:
-        game_duration += 1
-        if game_duration % 1000 == 0:
-            hero.sprite.bullets += 1
-
+        score += 1
         c = choice(['evil', 'zombie', 'godzilla'])
-        if not randint(0, 300) and game_duration < 5000:
-            enemies.add(Enemy(c))
-        if not randint(0, 200) and 5000 < game_duration < 10000:
-            enemies.add(Enemy(c))
-        if not randint(0, 100) and game_duration > 10000:
-            enemies.add(Enemy(c))
-
+        score_logic()
         draw_and_update(hero, enemies, bullets_hero, bullets_enemy, screen=screen)
 
         if spritecollide(hero.sprite, enemies, False):
@@ -257,15 +297,21 @@ while True:
 
         bullets_count_surface = font_render(str(hero.sprite.bullets), 50, True, BLACK)
         health_count = font_render(str(hero.sprite.health), 50, True, RED)
+        score_surface = font_render(f'SCORE: {str(score // 10)}', 60, True, FIOL)
 
         screen.blit(bullets_count_surface, (50, 30))
         screen.blit(bullets_surface, (95, 40))
         screen.blit(health_count, (160, 30))
         screen.blit(health_surface, (210, 42))
+        screen.blit(score_surface, (400, 20))
     else:
-        game_duration = 0
-        screen.blit(gave_over_surface, (365, 300))
-        screen.blit(start_text_surface, (265, 430))
+
+        if trophies_draw():
+            screen.blit(trophies_draw(), (480, 310))
+        screen.blit(text_surface, (420, 30))
+        screen.blit(gave_over_surface, (365, 100))
+        screen.blit(score_surface, (400, 210))
+        screen.blit(start_text_surface, (265, 500))
         enemies.empty()
         bullets_hero.empty()
         bullets_enemy.empty()
@@ -273,6 +319,7 @@ while True:
 
         if keys[pygame.K_SPACE]:
             hero.add(Hero())
+            score = 0
             game_active = True
 
     pygame.display.update()
